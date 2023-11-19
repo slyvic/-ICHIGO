f9db66ea09147d0c8cb3f606ab05228258e56f5d
rogerson <144379564+rogerson118701@users.noreply.github.com>
2023-11-19 22:28:26 +0900

    Update CustomerDetails.tsx

--- client/src/components/Customers/CustomerDetails.tsx
+++ client/src/components/Customers/CustomerDetails.tsx
@@ -1,45 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Tier from "../Tier";
 
+//Customer オブジェクトのインターフェースを定義する
 interface Customer {
-  customerId: string;
-  customerName: string;
-  tierName: string;
-  startDate: string;
-  totalSpent: number;
-  thisYearSpent: number;
-  amountForNextTier: number;
-  nextYearTier: string;
-  endDate: string;
-  amountForKeepTier: number;
+  customerId: string; // 顧客ID
+  customerName: string; // 顧客名
+  tierName: string; // ティア名
+  startDate: string; // 開始日
+  totalSpent: number; // 総支出額
+  thisYearSpent: number; // 今年の支出額
+  amountForNextTier: number; // 次のティアの金額
+  nextYearTier: string; // 来年のティア
+  endDate: string; // 終了日
+  amountForKeepTier: number; // ティアを維持するための金額
 }
 
 const CustomerDetails: React.FC = () => {
+  // URLパラメータからcustomerIdを取得します
   const { customerId } = useParams<{ customerId: string }>();
+
+  // customerステート変数とそのセッター関数を定義します
   const [customer, setCustomer] = useState<Customer | null>(null);
 
+  // 顧客IDを使用して顧客の詳細を取得する関数を定義する
+  const fetchCustomer = useCallback(async () => {
+    try {
+      const response = await axios.get(`http://localhost:8000/api/customers/${customerId}`);
+      setCustomer(response.data);
+    } catch (error) {
+      console.error('注文の取得中にエラーが発生しました:', error);
+    }
+  }, [customerId])
+
   useEffect(() => {
-    const fetchCustomer = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8000/api/customers/${customerId}`);
-        setCustomer(response.data);
-      } catch (error) {
-        console.error('注文の取得中にエラーが発生しました:', error);
-      }
-    };
-
     fetchCustomer();
-  }, [customerId]);
-
+  }, [fetchCustomer]);
+  
+  // 顧客データがまだ読み込み中の場合、ローディングメッセージを表示します
   if (!customer) {
     return <div>読み込み中...</div>;
   }
-  
-  const progressbarwidth = (customer.thisYearSpent / 500) * 100
 
-  // Render the component with the data
+  // プログレスバーの幅を計算します（thisYearSpentを500で割った比率）
+  const progressbarwidth = (customer.thisYearSpent / 500) * 100;
+
+  // 顧客データを使ってコンポーネントをレンダリングします
   return (
     <>
       <p className='headerTitle'>注文詳細</p>
@@ -55,11 +62,9 @@ const CustomerDetails: React.FC = () => {
         <p>終了日: {customer.endDate}</p>
         <p>キープ層の金額: ${customer.amountForKeepTier}</p>
         <div className='flex'>
-
-          <div className='meter'>
+          <div className={`meter`}>
             <span style={{ width: `${progressbarwidth}%` }}></span>
           </div>
-
         </div>
       </div>
     </>
